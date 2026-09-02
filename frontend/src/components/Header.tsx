40506d948096b022b1e008477a7475aaba7bba72
Felix Asatryan <felix21@MacBook-Air-Felix.local>
2026-09-02 21:51:04 +0300

    Add admin type to Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 interface Props {
-  currentPage: 'home' | 'dashboard' | 'login' | 'signup' | 'trading' | 'challenges'
-  setCurrentPage: (page: 'home' | 'dashboard' | 'login' | 'signup' | 'trading' | 'challenges') => void
+  currentPage: 'home' | 'dashboard' | 'login' | 'signup' | 'trading' | 'challenges' | 'admin'
+  setCurrentPage: (page: 'home' | 'dashboard' | 'login' | 'signup' | 'trading' | 'challenges' | 'admin') => void
   onLogout: () => void
 }
 
@@ -27,14 +27,20 @@ export default function Header({ setCurrentPage, onLogout }: Props) {
           className="btn-dashboard"
           onClick={() => setCurrentPage('challenges')}
         >
-          Challenges
-        </button>
-        <button 
-          className="btn-logout"
-          onClick={onLogout}
-        >
-          Logout
-        </button>
+          CChallenges
+</button>
+<button 
+  className="btn-dashboard"
+  onClick={() => setCurrentPage('admin')}
+>
+  Admin
+</button>
+<button 
+  className="btn-logout"
+  onClick={onLogout}
+>
+  Logout
+</button>
       </nav>
     </header>
   )
